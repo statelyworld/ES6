a2ef08f968c79282d3dd53329d9657cd8811f257
Stately World <statelyworld@gmail.com>
2019-03-18 19:54:40 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('Welcome');
+
+
+var greeting="Good Morning";
+/*
+ES6 Introduce
+*/
+var flag=true;
+
+if(flag)
+{
+    let fname="Gaurav";
+    console.log(fname);
+}
+
+console.log(fname);
+// let
+// const
